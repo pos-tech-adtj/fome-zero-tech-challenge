839a3bab1dcbd3916c0b6d1586a18e9150f281ca
Rafael.Bezerra <88746416+Rafazls@users.noreply.github.com>
2026-04-15 18:46:59 -0300

    TC-6 Criação da entidade e repositório & Fix: Correção das migrations… (#3)
    * TC-6 Criação da entidade e repositório & Fix: Correção das migrations do banco e otimização das imagens
    
    * feat: Incluindo relacionamento 1:1 de endereço com usuario & anotações do Hibernate

--- src/main/java/com/fiap/fomezero/domain/model/Usuario.java
+++ src/main/java/com/fiap/fomezero/domain/model/Usuario.java
@@ -1,4 +1,71 @@
 package com.fiap.fomezero.domain.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usuarios_seq")
+    @SequenceGenerator(name = "usuarios_seq", sequenceName = "usuarios_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String login;
+
+    /* 255 caracteres para senha!! - Ver se é necessário */
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "tipo_usuario", nullable = false, columnDefinition = "tipo_usuario_enum")
+    private TipoUsuario tipoUsuario;
+
+    @Column(name = "data_ultima_alteracao", nullable = false)
+    private LocalDateTime dataUltimaAlteracao;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private Endereco endereco;
 }
