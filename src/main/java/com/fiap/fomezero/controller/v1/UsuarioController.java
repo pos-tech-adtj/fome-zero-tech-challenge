5eee52cc372aa17e997b0a4ba7bf2e05cf296067
tainahiga <t.higa@reply.com>
2026-04-17 14:32:01 -0300

    TC-10 Implementação do endpoint PUT /usuarios/{id} para atualização de informações do usuário

--- src/main/java/com/fiap/fomezero/controller/v1/UsuarioController.java
+++ src/main/java/com/fiap/fomezero/controller/v1/UsuarioController.java
@@ -1,16 +1,14 @@
 package com.fiap.fomezero.controller.v1;
 
 import com.fiap.fomezero.dto.request.UsuarioCreateRequest;
+import com.fiap.fomezero.dto.request.UsuarioUpdateRequest;
 import com.fiap.fomezero.dto.response.UsuarioResponse;
 import com.fiap.fomezero.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/usuarios")
@@ -25,5 +23,13 @@ public class UsuarioController {
         UsuarioResponse usuario = usuarioService.criarUsuario(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioUpdateRequest request) {
+
+        UsuarioResponse usuario = usuarioService.atualizarUsuario(id, request);
+
+        return ResponseEntity.ok(usuario);
     }
 }
--- src/main/java/com/fiap/fomezero/exception/UsuarioNaoEncontradoException.java
+++ src/main/java/com/fiap/fomezero/exception/UsuarioNaoEncontradoException.java
@@ -1,4 +1,10 @@
 package com.fiap.fomezero.exception;
 
-public class UsuarioNaoEncontradoException {
+public class UsuarioNaoEncontradoException extends RuntimeException {
+
+    private static String message = "Nenhum usuário encontrado com o id informado";
+
+    public UsuarioNaoEncontradoException() {
+        super(message);
+    }
 }
