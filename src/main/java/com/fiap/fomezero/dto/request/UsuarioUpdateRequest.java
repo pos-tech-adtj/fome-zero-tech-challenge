dfbff5e97d981926770b1cb79cee8f42cbe57cd9
Tainá Higa <62307844+tainahiga@users.noreply.github.com>
2026-04-19 01:55:39 -0300

    TC-10 Implementação do endpoint PUT /usuarios/{id} para atualização de informações do usuário (#5)
    Co-authored-by: tainahiga <t.higa@reply.com>

--- src/main/java/com/fiap/fomezero/dto/request/UsuarioUpdateRequest.java
+++ src/main/java/com/fiap/fomezero/dto/request/UsuarioUpdateRequest.java
@@ -1,4 +1,21 @@
 package com.fiap.fomezero.dto.request;
 
-public class UsuarioUpdateRequest {
+import com.fiap.fomezero.domain.model.TipoUsuario;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+
+public record UsuarioUpdateRequest(
+
+        String nome,
+
+        @Email
+        String email,
+
+        String login,
+
+        TipoUsuario tipoUsuario,
+
+        @Valid
+        EnderecoRequest endereco
+) {
 }
