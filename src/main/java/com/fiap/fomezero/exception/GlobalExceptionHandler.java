7f310f8af11f0b79796718ca17716b3d23c534bf
Rafael Bezerra <rafaelsouza2017a@gmail.com>
2026-04-28 19:53:06 -0300

    TC-20 - Fix: Adicionando o import GetMapping e remove @ExceptionHandler duplicado, além de validar as colletions do postman

--- src/main/java/com/fiap/fomezero/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fiap/fomezero/exception/GlobalExceptionHandler.java
@@ -70,14 +70,4 @@ public class GlobalExceptionHandler {
 
         return problem;
     }
-
-    @ExceptionHandler(UsuarioNaoEncontradoException.class)
-    public ProblemDetail usuarioNaoEncontradoPorId(UsuarioNaoEncontradoException e) {
-
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-        problemDetail.setTitle("Usuário não encontrado");
-        problemDetail.setDetail(e.getMessage());
-
-        return problemDetail;
-    }
 }
